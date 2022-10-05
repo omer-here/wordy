c3d4da0187cca5aadb94bb1037402c35ecd4e120
omer-here <110015265+omer-here@users.noreply.github.com>
2022-10-04 19:22:00 -0700

    after removing router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router, 
-  Routes,
-  Route,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router, 
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 
  
 function App() {
@@ -39,22 +39,24 @@ function App() {
   }
   return (
     <>
-    <Router>
     <Navbar title="Wordy" mode={mode} toggleMode={toggleMode} key={new Date()} />
     <Alert alert={alert}/>
-    <div className="container my-3">
-    <Routes>
+    <TextForm showAlert={showAlert} heading="Try Wordy - word counter, character counter, remove extra spaces" mode={mode}/>
+    {/* <Router> */}
+   
+    {/* <div className="container my-3"> */}
+    {/* <Routes> */}
     {/* {/* /users --> Component 1
         /users/home --> Component 2 */}
-          <Route path="/About"
+          {/* <Route path="/About"
            element= {<About mode={mode} />}
-          />
-          <Route path="/"
+          /> */}
+          {/* <Route path="/"
           element= {<TextForm showAlert={showAlert} heading="Try Wordy - word counter, character counter, remove extra spaces" mode={mode}/>}
           />
     </Routes>
-    </div>
-    </Router>
+    </div> */}
+    {/* </Router> */}
     </> 
   );
 }
